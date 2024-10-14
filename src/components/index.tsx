9bea35e6cf80eadf07ba172a0a41d04c9442b6b0
Daniel <dramosv@smu.cl>
2024-10-14 17:22:46 -0300

    Fix build

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
+import '../dist/tailwind.css';
+
 export * from "./atoms/Button";
 export * from "./atoms/Input";
 export * from "./atoms/Text";
--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
-import { defineConfig } from 'vite'
-import path from "path";
-import react from '@vitejs/plugin-react-swc'
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, "./src/components/index.tsx"),
-      name: "PulitoDesign",
-      fileName: "pulito-design",
+      entry: path.resolve(__dirname, './src/components/index.tsx'),
+      name: 'PulitoDesign',
+      fileName: 'pulito-design',
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
