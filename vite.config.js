1a534acdf5f9b51ad69bb4e0ad64efc373f10ab0
Daniel <dramosv@smu.cl>
2024-10-14 15:57:15 -0300

    Create env for build

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import path from "path";
+import react from '@vitejs/plugin-react-swc';
+import dts from "vite-plugin-dts";
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "./src/components/index.tsx"),
+            name: "SimpleUI",
+            fileName: "simple-ui",
+        },
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                },
+            },
+        },
+    },
+    plugins: [react(), dts({ rollupTypes: true })],
+});
